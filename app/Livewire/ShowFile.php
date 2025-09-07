1b998628e6583bd72f3d0659d0cf8d4f47d651d2
Rob Pungello <rob@rpun.io>
2025-09-07 12:37:38 -0400

    Implement view file page

--- app/Livewire/ShowFile.php
+++ app/Livewire/ShowFile.php
@@ -2,11 +2,14 @@
 
 namespace App\Livewire;
 
+use App\Models\File;
 use Illuminate\Contracts\View\View;
 use Livewire\Component;
 
 class ShowFile extends Component
 {
+    public File $file;
+
     public function render(): View
     {
         return view('livewire.show-file');
--- resources/views/livewire/show-file.blade.php
+++ resources/views/livewire/show-file.blade.php
@@ -1,3 +1,27 @@
-<div>
+<div class="space-y-4">
+    <div>
+        <flux:heading size="xl" level="1">
+            {{ $file->title }}
+        </flux:heading>
+        <flux:heading size="lg" level="2" class="mb-4">
+            {{ $file->filename }} ({{ \Illuminate\Support\Number::fileSize($file->size)  }})
+        </flux:heading>
+    </div>
 
+    <div>
+        @foreach($file->tags as $tag)
+            <flux:badge>
+                {{ $tag->name }}
+            </flux:badge>
+        @endforeach
+    </div>
+
+    <flux:input :value="$file->download_short_url ?: $file->getDownloadUrl()" readonly copyable />
+
+    <flux:button variant="primary"
+                 icon="arrow-down-tray"
+                 :href="$file->getDownloadUrl()"
+    >
+        {{ __('Download') }}
+    </flux:button>
 </div>
