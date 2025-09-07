31d2d92bd4193d90be871c4da398f5e183cbdd3c
Rob Pungello <rob@rpun.io>
2025-09-07 14:36:11 -0400

    UI tweaks

--- resources/views/livewire/manage-files.blade.php
+++ resources/views/livewire/manage-files.blade.php
@@ -25,18 +25,15 @@
                     <flux:table.cell>{{ \Illuminate\Support\Number::fileSize($file->size) }}</flux:table.cell>
                     <flux:table.cell class="hidden xl:table-cell">{{ $file->created_at->format('F j, Y g:ia') }}</flux:table.cell>
                     <flux:table.cell>
-                        <flux:button variant="primary"
-                                     size="sm"
+                        <flux:button size="sm"
                                      :href="route('files.show', $file)"
                                      icon="eye"
                         />
-                        <flux:button variant="primary"
-                                     size="sm"
+                        <flux:button size="sm"
                                      :href="route('files.download', ['file' => $file->getKey(), 'token' => $file->download_token])"
                                      icon="arrow-down-tray"
                         />
-                        <flux:button variant="primary"
-                                     size="sm"
+                        <flux:button size="sm"
                                      icon="share"
                                      wire:click="share({{ $file->getKey() }})"
                         />
