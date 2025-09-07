1023e16abd2ca683bee6c749f9876a3a117e6101
Rob Pungello <rob@rpun.io>
2025-09-07 12:40:31 -0400

    Show downloads

--- resources/views/livewire/show-file.blade.php
+++ resources/views/livewire/show-file.blade.php
@@ -24,4 +24,24 @@
     >
         {{ __('Download') }}
     </flux:button>
+
+    <div>
+        <flux:heading size="lg" level="2">
+            {{ __('Downloads') }}
+        </flux:heading>
+        <flux:table>
+            <flux:table.columns>
+                <flux:table.column>{{ __('IP Address') }}</flux:table.column>
+                <flux:table.column>{{ __('Timestamp') }}</flux:table.column>
+            </flux:table.columns>
+            <flux:table.rows>
+                @foreach($file->downloads()->orderByDesc('created_at')->get() as $download)
+                    <flux:table.row>
+                        <flux:table.cell>{{ $download->ip_address }}</flux:table.cell>
+                        <flux:table.cell>{{ $download->created_at->format('F j, Y g:ia') }}</flux:table.cell>
+                    </flux:table.row>
+                @endforeach
+            </flux:table.rows>
+        </flux:table>
+    </div>
 </div>
