3d1c9e2bcb2073a64a43723258e7f8a7451df63f
Rob Pungello <rob@rpun.io>
2025-09-08 19:01:39 -0400

    Add missing foreign keys

---
+++ database/migrations/2025_09_08_190053_add_foreign_keys_to_file_requests_table.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('file_requests', function (Blueprint $table) {
+           $table->foreign('user_id')->references('id')->on('users')->cascadeOnDelete();
+           $table->foreign('folder_id')->references('id')->on('folders')->nullOnDelete();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('file_requests', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['folder_id']);
+        });
+    }
+};
