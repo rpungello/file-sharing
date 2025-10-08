06cb3db69ec23158b1692d0ed2d82e9daa1dff67
Rob Pungello <rob@rpun.io>
2025-10-08 12:10:17 -0400

    Disable nightwatch for /up route

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Foundation\Events\DiagnosingHealth;
+use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Nightwatch\Facades\Nightwatch;
 use Lorisleiva\Actions\Facades\Actions;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,5 +26,9 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->runningInConsole()) {
             Actions::registerCommands();
         }
+
+        Event::listen(function (DiagnosingHealth $event) {
+            Nightwatch::dontSample();
+        });
     }
 }
