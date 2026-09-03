425e8d44db7dc58be3f4322fea7af8c9fda6811b
Rob Pungello <rob@rpun.io>
2026-09-03 11:52:26 -0400

    Allow null user_agent in downloads table

---
+++ database/migrations/2026_09_03_115145_change_user_ageent_in_downloads_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('downloads', function (Blueprint $table) {
+            $table->string('user_agent')->nullable()->change();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('downloads', function (Blueprint $table) {
+            $table->string('user_agent')->nullable(false)->change();
+        });
+    }
+};
